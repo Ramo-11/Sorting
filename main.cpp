68be094139a20b4fbdc18a21aa6f2b2491f208e9
Omar <oabdela@iu.edu>
2021-04-11 15:42:55 -0400

    Generates Random Numbers

--- main.cpp
+++ main.cpp
@@ -1,48 +1,81 @@
 #include <iostream>
+#include <Vector>
+#include <cstdlib>
+#include <ctime>
 
-void InsertionSort(int *array, int n);
-void PrintArray(int *array, int n);
+void InsertionSort(std::vector<int> *arr);
+void MergeSort(std::vector<int> *arr);
+
+void PrintArray(std::vector<int> *arr);
+std::vector<int> GenerateRandomNum();
 
 int main()
 {
-    int arr[5] = { 12, 11, 13, 5, 6 };
+    std::vector<int> arr = GenerateRandomNum();
 
-    PrintArray(arr, 5);
+    std::cout << "\nBefore Sorting";
+    PrintArray(&arr);
 
-    InsertionSort(arr, 5);
+    InsertionSort(&arr);
 
-    PrintArray(arr, 5);
+    std::cout << "\n\nAfter Sorting";
+    PrintArray(&arr);
 
     std::cout << "\n";
     return 0;
 }
 
-void InsertionSort(int *array, int n)
+void InsertionSort(std::vector<int> *arr)
 {
     int numsBeforeKey;
     int key;
 
-    for(int i = 1; i < n; i++)
+    for(int i = 1; i < arr->size(); i++)
     {
-        key = array[i];
+        key = (*arr)[i];
         numsBeforeKey = i - 1;
 
         //swap all of the elements between key and all of the elements before it
-        for(numsBeforeKey; numsBeforeKey >= 0 && array[numsBeforeKey] > key; numsBeforeKey--)
+        for(numsBeforeKey; numsBeforeKey >= 0 && (*arr)[numsBeforeKey] > key; numsBeforeKey--)
         {
-            array[numsBeforeKey + 1] = array[numsBeforeKey];
+            (*arr)[numsBeforeKey + 1] = (*arr)[numsBeforeKey];
         }
 
         //This line makes sure the value that was just swapped is not lost
-        array[numsBeforeKey + 1] = key;
+        (*arr)[numsBeforeKey + 1] = key;
     }
 }
 
-void PrintArray(int *array, int n)
+void PrintArray(std::vector<int> *arr)
 {
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < arr->size(); i++)
     {
-        std::cout << array[i] << " ";
+        if(i % 10 == 0)
+            std::cout << "\n"; 
+        std::cout << (*arr)[i] << " ";
     }
     std::cout << "\n";
+}
+
+std::vector<int> GenerateRandomNum()
+{
+    int userChoice;
+    int randomNum;
+    std::vector<int> arr;
+
+    std::cout << "Enter a number to generate random numbers:\n";
+    std::cin >> randomNum;
+
+    std::srand(randomNum);
+
+    std::cout << "\nHow many numbers would you like to be generated?\n";
+    std::cin >> userChoice;
+    
+    for(int i = 0; i < userChoice; i++)
+    {
+        randomNum = rand() % 1000;
+        arr.push_back(randomNum);
+    }
+
+    return arr;
 }
